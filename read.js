86271912dbdae25b862d49370b21a092919578fb
sasham43 <sashasemail@gmail.com>
2017-02-20 16:18:44 -0600

    clean up

--- read.js
+++ read.js
@@ -4,12 +4,6 @@ var args = process.argv;
 
 console.log('reading...');
 
-// if(args[2] === '-v'){
-//   var cmd = 'ruby tape-read.rb | omxplayer pipe:0';
-// } else if (args[2] === '-y'){
-//   var cmd = 'ruby tape-read.rb';
-// }
-
 var cmd = 'ruby';
 var r_args = [
   'tape-read.rb'
@@ -37,32 +31,10 @@ tape.stdout.on('end', function(){
   });
 
   playlist.map(function(video, index){
-    console.log('video type:', video);
+    // console.log('video type:', video);
     console.log('playing', index + '...');
     video = video.toString().replace(/\r?\n|\r/g, ''); // remove line endings
     video_cmd = 'omxplayer \'' + video + '\'';
     cp.execSync(video_cmd);
-  })
-
-  // var youtube_dl = cp.exec('youtube-dl -g ' + playlist[0], function(err, url, stderr){
-  //   if(err){
-  //     console.log('err:', err);
-  //   } else {
-  //     // console.log('out:', url);
-  //     // replace line endings
-  //     url = url.replace(/\r?\n|\r/g, '');
-  //
-  //     var omx_cmd = 'omxplayer \'' + url + '\'';
-  //     console.log('omx_cmd:', omx_cmd);
-  //
-  //     var omxplayer = cp.exec(omx_cmd, function(err, stdout, stderr){
-  //       if(err){
-  //         console.log('err omx:', err);
-  //       } else {
-  //         console.log('stdout:', stdout);
-  //       }
-  //     });
-  //   }
-  // });
-
+  });
 });
