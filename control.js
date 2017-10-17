736c3b58f0f00cd2497d8ea6f707371c7708da2f
sasham43 <sashasemail@gmail.com>
2017-10-16 21:33:31 -0500

    f

--- control.js
+++ control.js
@@ -27,9 +27,12 @@ tape.stdout.on('end', function(){
   videos = playlist.split('\n');
   console.log('loading urls...');
   var url_promises = [];
+
+  // get urls that omxplayer can play
   videos.forEach(function(video){
     var quote_re = /\"/g;
     video = video.replace(quote_re, '');
+    console.log('video', video);
 
     if(video.includes("https://www.youtube.com")){
       var get_url = 'youtube-dl -f mp4 -g ' + video;
@@ -39,43 +42,44 @@ tape.stdout.on('end', function(){
       var title_promise = q.ninvoke(cp, 'execSync', get_title);
       url_promises.push(url_promise);
       url_promises.push(title_promise);
-
-      q.allSettled(url_promises).then(function(responses){
-        var playlist = [];
-        var choices = [];
-
-        var item = {};
-        responses.forEach(function(r, i){
-          if(i == 0 || item % 2 == 0){
-            item.url = r;
-            choices.push(r);
-          }else {
-            item.title = r;
-            playlist.push(item);
-            item = {};
-          }
-        });
-
-        inquirer.prompt({
-          type: 'list',
-          name: 'song',
-          message: 'play a video',
-          choices: choices
-        }).then(function(answers){
-          console.log('answers:', answers);
-
-          var song = _.find(playlist, function(p){
-            return p.title = answers.song;
-          });
-
-          cp.exec('omxplayer \'' + song.url + '\'');
-        }).catch(function(err){
-          console.log('failed at erroring:', err);
-        });
-
-      }).catch(function(err){
-        console.log('did an oopsie:', err);
-      });
     }
+  });
+
+  // build playlist, open inquirer, play video
+  q.allSettled(url_promises).then(function(responses){
+    var playlist = [];
+    var choices = [];
+
+    var item = {};
+    responses.forEach(function(r, i){
+      if(i == 0 || item % 2 == 0){
+        item.url = r;
+        choices.push(r);
+      }else {
+        item.title = r;
+        playlist.push(item);
+        item = {};
+      }
+    });
+
+    inquirer.prompt({
+      type: 'list',
+      name: 'song',
+      message: 'play a video',
+      choices: choices
+    }).then(function(answers){
+      console.log('answers:', answers);
+
+      var song = _.find(playlist, function(p){
+        return p.title = answers.song;
+      });
+
+      cp.exec('omxplayer \'' + song.url + '\'');
+    }).catch(function(err){
+      console.log('failed at erroring:', err);
+    });
+
+  }).catch(function(err){
+    console.log('did an oopsie:', err);
   });
 })
