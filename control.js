6eda5b3b8f62efee6d1e007e9458119e42127246
sasham43 <sashasemail@gmail.com>
2017-10-20 21:42:58 -0500

    indent

--- control.js
+++ control.js
@@ -23,70 +23,71 @@ function listen(){
   var q = require('q');
   var inquirer = require('inquirer');
   tape.stdout.on('end', function(){
-    console.log(playlist);
+      console.log(playlist);
 
-    videos = playlist.split('\n');
-    console.log('loading urls...');
-    var url_promises = [];
+      videos = playlist.split('\n');
+      console.log('loading urls...');
+      var url_promises = [];
 
-    // get urls that omxplayer can play
-    videos.forEach(function(video){
-      var quote_re = /\"/g;
-      video = video.replace(quote_re, '');
-      console.log('video', video.length);
+      // get urls that omxplayer can play
+      videos.forEach(function(video){
+        var quote_re = /\"/g;
+        video = video.replace(quote_re, '');
+        console.log('video', video.length);
 
-      if((video.includes("https://www.youtube.com") && (video.length == 43))){
-        var get_url = 'youtube-dl -f mp4 -g ' + video;
-        var get_title = 'youtube-dl -e ' + video;
+        if((video.includes("https://www.youtube.com") && (video.length == 43))){
+          var get_url = 'youtube-dl -f mp4 -g ' + video;
+          var get_title = 'youtube-dl -e ' + video;
 
-        var url_promise = q.ninvoke(cp, 'exec', get_url);
-        var title_promise = q.ninvoke(cp, 'exec', get_title);
-        url_promises.push(url_promise);
-        url_promises.push(title_promise);
-      }
-    });
+          var url_promise = q.ninvoke(cp, 'exec', get_url);
+          var title_promise = q.ninvoke(cp, 'exec', get_title);
+          url_promises.push(url_promise);
+          url_promises.push(title_promise);
+        }
+      });
 
-    // build playlist, open inquirer, play video
-    q.allSettled(url_promises).then(function(responses){
+      // build playlist, open inquirer, play video
+      q.allSettled(url_promises).then(function(responses){
 
-      if(response.length > 0){
-        var playlist = [];
-        var choices = [];
+        if(response.length > 0){
+          var playlist = [];
+          var choices = [];
 
-        var item = {};
+          var item = {};
 
-        responses.forEach(function(r,i){
-          if(i == 0 || item % 2 == 0){
-            item.title = r;
-            playlist.push(item);
-            item = {};
-          } else {
-            item.url = r;
-            choices.push(r);
-          }
-        });
-
-        inquirer.prompt({
-          type: 'list',
-          name: 'song',
-          message: 'play a video',
-          choices: choices
-        }).then(function(answers){
-          console.log('answers:', answers);
-
-          var song = _.find(playlist, function(p){
-            return p.title == answers.song;
+          responses.forEach(function(r,i){
+            if(i == 0 || item % 2 == 0){
+              item.title = r;
+              playlist.push(item);
+              item = {};
+            } else {
+              item.url = r;
+              choices.push(r);
+            }
           });
 
-          cp.exec('omxplayer \'' + song.url + '\'');
-        }).catch(function(err){
-          console.log('failed at erroring:', err);
-        });
-      } else {
-        listen();
-      }
-  })
-  .catch(function(err){
-    console.log('did an oopsie:', err);
+          inquirer.prompt({
+            type: 'list',
+            name: 'song',
+            message: 'play a video',
+            choices: choices
+          }).then(function(answers){
+            console.log('answers:', answers);
+
+            var song = _.find(playlist, function(p){
+              return p.title == answers.song;
+            });
+
+            cp.exec('omxplayer \'' + song.url + '\'');
+          }).catch(function(err){
+            console.log('failed at erroring:', err);
+          });
+        } else {
+          listen();
+        }
+    })
+    .catch(function(err){
+      console.log('did an oopsie:', err);
+    });
   });
 }
