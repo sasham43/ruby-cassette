9dcb39955452c4daf6de5157df590c5f534ae2c6
sasham43 <sashasemail@gmail.com>
2017-10-27 18:17:40 -0500

    made a writer

--- write.js
+++ write.js
@@ -2,14 +2,32 @@ var cp = require('child_process');
 
 var args = process.argv;
 
-console.log('listening...');
+console.log('writing...');
 
 var playlist_file = 'playlist2.txt';
 var cmd = 'ruby';
 var r_args = [
-  'tape-write.rb',
+  './tape-write.rb',
   '<',
   playlist_file
 ];
 
-var tape = cp.spawn(cmd, r_args);
+// for (var i = 0; i < 300 ; i++){
+//   // var tape = cp.spawn(cmd, r_args);
+//     cp.spawnSync(cmd, r_args);
+//
+//     setTimeout(function(){
+//       console.log('in a timeout...', 3000)
+//     },2000)
+// }
+
+
+setInterval(function(){
+  // cp.spawnSync(cmd, r_args);
+  cp.execSync(r_args.join(' '))
+},1000)
+
+//
+// setTimeout(function(){
+//   console.log('thing')
+// },3000)
